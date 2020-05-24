fe5ea6b45c381cdcc745cb214fbd20f8ab981d7f
Glitch (hello-webpage) <none>
2020-05-24 01:42:30 +0000

    🐕⏱ Checkpoint

---
+++ public/script.js
@@ -0,0 +1,166 @@
+// client-side js, loaded by index.html
+// run by the browser each time the page is loaded
+
+const url = "wss://glitch-websocket-chat.glitch.me";
+const connection = new WebSocket(url);
+
+let e = document.getElementById("newMsg");
+e.addEventListener("change", sendNewMsg);
+
+function sendNewMsg() {
+  let e = document.getElementById("newMsg");
+  let msgObj = {
+    "type": "message",
+    "from": "host",
+    "msg": e.value
+  }
+  connection.send(JSON.stringify(msgObj));
+  e.value = null;
+}
+
+let addMessage = function(message) {
+  const pTag = document.createElement("p");
+  pTag.appendChild(document.createTextNode(message));
+  document.getElementById("messages").appendChild(pTag);
+};
+
+connection.onopen = () => {
+  connection.send(JSON.stringify({"type":"helloHost"}));
+};
+
+connection.onerror = error => {
+  console.log(`WebSocket error: ${error}`);
+};
+
+connection.onmessage = event => {
+  let msgObj = JSON.parse(event.data);
+  if (msgObj.type == "message") {
+    addMessage(msgObj.from+": "+msgObj.msg);
+  } else {
+    addMessage(msgObj.type);
+  }
+};
+
+
+
+document.querySelector('#go').addEventListener('click', () => {
+ 
+  let location = document.getElementById("location").value;
+  let search_word = document.getElementById("autocompletelist").options[0].value;
+
+ 
+  alert(location);
+   alert(search_word);
+  // new HttpRequest instance 
+  var xmlhttp = new XMLHttpRequest();   
+  xmlhttp.open("POST", '/getRestaurant');
+  // important to set this for body-parser
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  // setup callback function
+   
+  xmlhttp.onloadend = function(e) {
+  console.log(xmlhttp.responseText);
+  let data = JSON.parse(xmlhttp.responseText);
+    
+ document.getElementById("restaurantImg").src=data[0].image_url;
+   // document.getElementById("restaurantImg").src="https://s3-media2.fl.yelpcdn.com/bphoto/LTfgfyCJaboZdhHWemxl5A/o.jpg"
+   
+  alert(xmlhttp.responseText);
+  }
+  // all set up!  Send off the HTTP request
+  xmlhttp.send(JSON.stringify({ "param1":location , "param2":search_word }));
+});
+
+//reviews();
+function reviews() {
+  let url = "reviews";
+  
+  let xhr = new XMLHttpRequest;
+  xhr.open("GET",url);
+  // Next, add an event listener for when the HTTP response is loaded
+  xhr.addEventListener("load", function() {
+      if (xhr.status == 200) {
+        let responseStr = xhr.responseText;  // get the JSON string 
+        let gList = JSON.parse(responseStr);  // turn it into an object
+    //    display(gList);  // print it out as a string, nicely formatted
+      } else {
+        console.log(xhr.responseText);
+      }
+  });
+  // Actually send request to server
+  xhr.send();
+}
+
+let keyword = document.getElementById("keyword");
+keyword.addEventListener("change", autoComplete);
+
+  //let msg = document.querySelector('#message');
+ // let img = document.querySelector('#cardImg');
+ // let selected_image = document.querySelector('#selected_image');
+//autoComplete();
+  function autoComplete() {
+  
+  let url = "/autoComplete";
+  let search_word = document.getElementById("keyword").value;
+  let data = {
+    "keyword": search_word
+  }
+  //alert(data.keyword);
+  console.log(data.keyword);
+  let xhr = new XMLHttpRequest;
+  xhr.open("POST",url);
+    
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  // setup callback function
+  xhr.onloadend = function(e) {
+    //console.log(e);
+    console.log(xhr.responseText);         
+   // show_popup( xhr.responseText);
+    let responseStr = xhr.responseText;  // get the JSON string 
+  //  alert(responseStr);
+    let wordList = JSON.parse(responseStr);  // turn it into an object
+      
+    let dataList = document.getElementById("autocompletelist");
+        
+         // Loop over the JSON array.
+    wordList.forEach(function(item) {
+        // Create a new <option> element.
+        var option = document.createElement('option');
+        // Set the value using the item in the JSON array.
+        option.value = item.text;
+      //  alert(item.text);
+        // Add the <option> element to the <datalist>.
+        dataList.appendChild(option);
+      });
+      } 
+ 
+      
+  
+ 
+  // Actually send request to server
+xhr.send(JSON.stringify({ "keyword":search_word }));
+}
+  // all set up!  Send off the HTTP request
+//  xmlhttp.send(JSON.stringify(data));
+
+//businessdetails();
+  function businessdetails() {
+   
+  let url = "businessdetails";
+  
+  let xhr = new XMLHttpRequest;
+  xhr.open("GET",url);
+  // Next, add an event listener for when the HTTP response is loaded
+  xhr.addEventListener("load", function() {
+      if (xhr.status == 200) {
+        let responseStr = xhr.responseText;  // get the JSON string 
+        let gList = JSON.parse(responseStr);  // turn it into an object
+    //    display(gList);  // print it out as a string, nicely formatted
+      } else {
+        console.log(xhr.responseText);
+      }
+  });
+  // Actually send request to server
+  xhr.send();
+}
+  
---
+++ server.js
@@ -0,0 +1,139 @@
+const WebSocket = require('ws');
+const express = require("express");
+const app = express();
+const http = require("http");
+app.use(express.json());
+// Multer is a module to read and handle FormData objects, on the server side
+const multer = require('multer');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public")); 
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
+});
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // console.log(message)
+    //ws.send("server echo:" + message);
+    broadcast(message)
+  })
+  ws.send('connected!')
+})
+
+function broadcast(data) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}
+
+//start our server
+server.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
+
+
+const yelp = require('yelp-fusion');
+
+const apiKey = process.env.YELP_API_KEY;
+const client = yelp.client(apiKey);
+
+
+
+
+app.post('/getRestaurant', function(req, res){
+ 
+
+  var location = req.body.param1;
+  var keywords = req.body.param2;
+  console.log(location);
+  console.log(keywords);
+  const searchRequest = {
+ // term:'Four Barrel Coffee',
+  term: keywords,
+  categories:'food',
+  location: location
+  //location: 'Davis,ca'
+};
+
+
+client.search(searchRequest).then(response => {
+  
+  response.jsonBody.businesses.forEach(function(row) {
+  //  var result = row.businesses;
+     const prettyJson = JSON.stringify(row, null, 4);
+     console.log(prettyJson);
+});
+/*  const firstResult = response.jsonBody.businesses[0];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
+  
+   const secondResult = response.jsonBody.businesses[1];
+  const prettyJson2= JSON.stringify(secondResult, null, 4);
+  console.log(prettyJson2);
+  */
+//  const firstResult = response.jsonBody.businesses[0];
+  res.json(response.jsonBody.businesses);
+}).catch(e => {
+  console.log(e);
+});
+});
+
+
+app.post('/autoComplete', function(req, res, next){
+   
+    let keyword = req.body.keyword;
+    console.log(keyword);
+    client.autocomplete({
+      text: keyword
+    }).then(response => {
+      console.log(response.jsonBody.terms[0].text);
+  //      console.log(response.jsonBody.terms[1].text);//TODO: check length first
+       res.send(response.jsonBody.terms);
+    }).catch(e => {
+      console.log(e);
+    });
+
+});
+
+
+ app.get('/reviews', function(request, response, next){
+
+
+client.reviews('black-bear-diner-davis').then(response => {
+  console.log(response.jsonBody.reviews[0].text);
+   console.log(response.jsonBody.reviews[1].text);
+
+}).catch(e => {
+  console.log(e);
+});
+
+});
+
+ app.get('/businessdetails', function(request, response, next){
+
+
+client.business('black-bear-diner-davis').then(response => {
+  console.log(response.jsonBody.name);
+  console.log(response.jsonBody.price);
+  console.log(response.jsonBody.rating);
+  console.log(response.jsonBody.review_count);
+  console.log(response.jsonBody.image_url);
+  console.log(response.jsonBody.url);
+ 
+  
+}).catch(e => {
+  console.log(e);
+});
+
+});
