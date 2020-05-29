09ed0fd4e4f6f4907c2d8d5bea8b91b3c2119d21
Glitch (hello-webpage) <none>
2020-05-29 22:32:08 +0000

    ☂️👸 Checkpoint ./public/index.html:13912260/371 ./public/script.js:13912260/12537 ./public/client.html:13912260/78

--- public/script.js
+++ public/script.js
@@ -24,6 +24,12 @@ let addMessage = function(message) {
   document.getElementById("messages").appendChild(pTag);
 };
 
+let displayProgress = function(message) {
+  const pTag = document.getElementById("progress");
+  pTag.textContent=message;
+ 
+};
+
 connection.onopen = () => {
   connection.send(JSON.stringify({"type":"helloHost"}));
 };
@@ -32,11 +38,32 @@ connection.onerror = error => {
   console.log(`WebSocket error: ${error}`);
 };
 
+let progressBar = document.getElementById("progress");
+let aRestaurant = document.getElementById("restaurant_page");
+
 connection.onmessage = event => {
   let msgObj = JSON.parse(event.data);
   if (msgObj.type == "message") {
     addMessage(msgObj.from+": "+msgObj.msg);
-  } else {
+  }
+  else if (msgObj.type == 'command') {
+ //   button1.textContent = msgObj.info[0];
+ //   button2.textContent = msgObj.info[1];
+    aRestaurant.textContent=msgObj.info;
+ 
+     let restaurant = msgObj.info;
+    //  alert('got another restaurant ' + restaurant);
+    //send AJAX request to server to get a restaurant
+     getRestaurant(restaurant);
+    
+  }
+  else if (msgObj.type == 'progress') {
+ 
+ 
+     displayProgress(msgObj.info);
+    
+  }
+  else {
     addMessage(msgObj.type);
   }
 };
@@ -133,6 +160,8 @@ let msgObj = {
  
 // window.location.href = "https://brainy-ionian-find.glitch.me/client.html";
 });
+
+
 document.querySelector('#go').addEventListener('click', () => {
  
   let location = document.getElementById("location").value;
@@ -159,7 +188,7 @@ document.querySelector('#go').addEventListener('click', () => {
     //clean up
     dataList.textContent="";
     // Loop over the JSON array.
-    restaurantList.forEach(function(item) {
+/*    restaurantList.forEach(function(item) {
            var gallery_div = document.createElement('div');
            gallery_div.className="gallery";
            var img = document.createElement('img');
@@ -400,7 +429,7 @@ document.querySelector('#go').addEventListener('click', () => {
            dataList.appendChild(gallery_div);
        
       });
-
+*/
  
   }
   // all set up!  Send off the HTTP request
@@ -456,4 +485,315 @@ function reviews() {
     
   
 
+}
+
+function getRestaurant(queryStringID){
+
+     
+          var xmlhttp = new XMLHttpRequest();   
+          xmlhttp.open("POST", '/getARestaurant');
+     
+          xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          // setup callback function
+          xmlhttp.onloadend = function(e) {
+       
+           let responseStr = xmlhttp.responseText;  // get the JSON string 
+
+           let  item = JSON.parse(responseStr);  // turn it into an object      
+           let dataList = document.getElementById("restaurant");
+            dataList.style.background = "#ff6b6b";
+           
+           let restaurantPage = document.getElementById("restaurant_page");
+         
+             
+            //clean up
+            dataList.textContent="";          
+            restaurantPage.textContent="";
+         
+            var yes_btn_div = document.createElement("div");          
+            var no_btn_div = document.createElement("div");
+            var yes_btn = document.createElement('img');    
+            var no_btn = document.createElement('img'); 
+            
+            
+            yes_btn.src="https://cdn.glitch.com/6fccf2e6-5025-49e3-bef1-5d3addbc924c%2FheartButton.svg?v=1590786125475";
+            no_btn.src="https://cdn.glitch.com/6fccf2e6-5025-49e3-bef1-5d3addbc924c%2FrejectButton.svg?v=1590786117124";
+            yes_btn.className="image-button";
+            no_btn.className="image-button"
+            yes_btn_div.appendChild(yes_btn);
+            no_btn_div.appendChild(no_btn);
+   
+            
+            yes_btn.addEventListener("click", () => {
+           let cmdObj= {
+             "type": "command",
+             "selection":1
+           }
+           connection.send(JSON.stringify(cmdObj));
+          });
+
+          no_btn.addEventListener("click", () => {
+             let cmdObj= {
+               "type": "command",
+               "selection":0
+             }
+
+             connection.send(JSON.stringify(cmdObj));
+          });
+
+           dataList.appendChild(no_btn_div);
+            
+           var gallery_div = document.createElement("div");
+           gallery_div.className="gallery";
+           var img = document.createElement("img");
+           img.src = item.image_url;
+     
+           gallery_div.appendChild(img);
+    
+   
+      
+           var desc_div = document.createElement("div");
+           desc_div.className="desc";
+           desc_div.textContent=item.name + " " + item.price ;
+            
+           var title_div = document.createElement("div");
+           title_div.className="title";
+           title_div.textContent=item.address;
+       
+           var  rating_div = document.createElement("div");
+           rating_div.className="rating";
+          
+       
+              
+           if(item.rating==5) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating3 = document.createElement('i');
+              rating3.className="fas fa-star"
+             
+              var rating4 = document.createElement('i');
+              rating4.className="fas fa-star"
+             
+              var rating5 = document.createElement('i');
+              rating5.className="fas fa-star"
+             
+             rating_div.appendChild(rating1);
+             rating_div.appendChild(rating2);
+             rating_div.appendChild(rating3);
+             rating_div.appendChild(rating4);
+             rating_div.appendChild(rating5);
+             
+           }
+           else if(item.rating==4) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating3 = document.createElement('i');
+              rating3.className="fas fa-star"
+             
+              var rating4 = document.createElement('i');
+              rating4.className="fas fa-star"
+             
+              var rating5 = document.createElement('i');
+              rating5.className="far fa-star"
+          
+              rating5.textContent=item.review_count + " reviews";
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating5);
+             
+           } 
+           else if(item.rating==4.5) {
+             
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+            
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating3 = document.createElement('i');
+              rating3.className="fas fa-star"
+             
+              var rating4 = document.createElement('i');
+              rating4.className="fas fa-star"
+             
+              var rating45 = document.createElement('i');
+              rating45.className="fas fa-star-half-alt"
+              rating45.textContent=item.review_count + " reviews";
+             
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating45);
+             
+             
+           } 
+           else if(item.rating==3) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating3 = document.createElement('i');
+              rating3.className="fas fa-star"
+          
+               var rating4 = document.createElement('i');
+              rating4.className="far fa-star"
+             
+              var rating5 = document.createElement('i');
+              rating5.className="far fa-star"
+              rating5.textContent=item.review_count + " reviews";
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating5);
+          
+           }
+           else if(item.rating==3.5) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating3 = document.createElement('div');
+              rating3.className="fas fa-star"
+             
+            
+             
+              var rating35 = document.createElement('i');
+                               
+              rating35.className="fas fa-star-half-alt"
+             
+              var rating4 = document.createElement('i');
+              rating4.className="far fa-star"
+              rating4.textContent=item.review_count + " reviews";
+             
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating35);
+              rating_div.appendChild(rating4);
+           }
+           else if(item.rating==2) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+              
+              var rating3 = document.createElement('i');
+              rating3.className="far fa-star"
+              var rating4 = document.createElement('i');
+              rating4.className="far fa-star"
+              var rating5 = document.createElement('i');
+              rating5.className="far fa-star"
+              rating5.textContent=item.review_count + " reviews";
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating5);
+            
+           }
+           else if(item.rating==2.5) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";
+             
+              var rating2 = document.createElement('i');
+              rating2.className="fas fa-star";
+             
+              var rating25 = document.createElement('i');
+              rating25.className="fas fa-star-half-alt"
+             
+              var rating4 = document.createElement('i');
+              rating4.className="far fa-star"
+              var rating5 = document.createElement('i');
+              rating5.className="far fa-star"
+              rating5.textContent=item.review_count + " reviews";
+             
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);           
+              rating_div.appendChild(rating25);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating5);
+             
+           }
+           else if(item.rating==1) {
+              var rating1 = document.createElement('i');
+              rating1.className="fas fa-star";      
+            
+              var rating2 = document.createElement('i');
+              rating2.className="far fa-star"
+              var rating3 = document.createElement('i');
+              rating3.className="far fa-star"
+           
+             
+             
+              var rating4 = document.createElement('i');
+              rating4.className="far fa-star"
+              var rating5 = document.createElement('i');
+              rating5.className="far fa-star"
+              rating5.textContent=item.review_count + " reviews";
+             
+              rating_div.appendChild(rating1);
+              rating_div.appendChild(rating2);
+              rating_div.appendChild(rating3);
+              rating_div.appendChild(rating4);
+              rating_div.appendChild(rating5);
+           
+           }
+           else if(item.rating==1.5) {
+             var rating1 = document.createElement('i');
+             rating1.className="fas fa-star";
+             
+             var rating15 = document.createElement('i');
+             rating15.className="fas fa-star-half-alt"
+             
+             var rating3 = document.createElement('i');
+             rating3.className="far fa-star"
+           
+             
+             
+             var rating4 = document.createElement('i');
+             rating4.className="far fa-star"
+             var rating5 = document.createElement('i');
+             rating5.className="far fa-star"
+             rating5.textContent=item.review_count + " reviews";
+             rating_div.appendChild(rating1);
+             rating_div.appendChild(rating15);
+             rating_div.appendChild(rating3);
+             rating_div.appendChild(rating4);
+             rating_div.appendChild(rating5);
+           }
+      
+           gallery_div.appendChild(desc_div);
+           gallery_div.appendChild( title_div);     
+           gallery_div.appendChild(rating_div);          
+           restaurantPage.appendChild(gallery_div);
+           dataList.appendChild(restaurantPage);
+           dataList.appendChild(yes_btn_div);
+        
+   //   });
+
+ 
+  
+
+  }
+  // all set up!  Send off the HTTP request
+ //    var data = JSON.stringify({ "name": name.value, "email": email.value }); 
+  
+  xmlhttp.send(JSON.stringify({ "queryID": queryStringID }));
 }
