56b03366f09d1936035f704d85ce5d29569479bd
Glitch (hello-webpage) <none>
2020-05-31 21:14:22 +0000

    🍈🌵 Checkpoint ./public/script.js:13912260/3 ./public/style.css:13912260/81

--- public/script.js
+++ public/script.js
@@ -395,10 +395,10 @@ function getRestaurant(queryStringID){
       
            var desc_div = document.createElement("div");
            desc_div.className="desc";
-           desc_div.textContent=item.name 
+           desc_div.textContent=item.name ;
             
             desc_container_div.appendChild(desc_div);
-          desc_container_div.appendChild(price_div);
+            desc_container_div.appendChild(price_div);
           
             
            var title_div = document.createElement("div");
