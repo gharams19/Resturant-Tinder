1ba739166be639b604c54174a7248e75a6d0b585
Glitch (hello-webpage) <none>
2020-06-01 23:53:19 +0000

    👮🎍 Checkpoint ./server.js:14173008/32 ./public/script.js:14173008/10

--- public/script.js
+++ public/script.js
@@ -371,7 +371,7 @@ function getRestaurant(queryStringID) {
     review_div.className = "review";
     review_div.textContent = "Review";
     review_div.addEventListener("click", () => {
-      reviews(item);
+      reviews(item.address);
     });
 
     var review_count_div = document.createElement("div");
